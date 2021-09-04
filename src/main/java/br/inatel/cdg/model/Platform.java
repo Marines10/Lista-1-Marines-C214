357e65efeaa48bf4806ae733c919df0e55008646
neneis <marines.ribeiro@gec.inatel.br>
2021-09-04 08:54:48 -0300

    "Rearrumando Lista"

---
+++ src/main/java/br/inatel/cdg/model/Platform.java
@@ -0,0 +1,6 @@
+package br.inatel.cdg.model;
+
+public enum Platform {
+
+    PC,XB,NitendoWii,GB,GBA,X360,PS3,PS2;
+}
---
+++ src/test/java/br/inatel/cdg/service/TesteServiceGame.java
@@ -0,0 +1,56 @@
+package br.inatel.cdg.service;
+
+import br.inatel.cdg.model.Game;
+import br.inatel.cdg.model.Platform;
+import br.inatel.cdg.model.Publisher;
+import br.inatel.cdg.utils.CsvUtils;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class TesteServiceGame {
+
+    private static List<Game> gameList;
+
+
+    @BeforeClass
+    public static void initClass() throws URISyntaxException {
+        Path caminho = Paths.get(ClassLoader.getSystemResource("vendas-games.csv").toURI());
+        gameList = CsvUtils.readGameCsv(caminho);
+    }
+
+    @Test
+    public void testequantidadeGamesNitendoWii(){
+        List<Game> ps4Games = ServiceGame.getListByPlatform(gameList, Platform.NitendoWii);
+
+        Assert.assertEquals(5,ps4Games.size());
+    }
+
+    @Test
+    public void testequantidadeGameX360(){
+        List<Game> x360Games = ServiceGame.getListByPlatform(gameList, Platform.X360);
+
+        Assert.assertEquals(16,x360Games.size());
+    }
+
+    @Test
+    public void testequantidadeGameActivision(){
+        List<Game> activisionGames = ServiceGame.getByPuBlisher(gameList, Publisher.Activision);
+
+        Assert.assertEquals(14,activisionGames.size());
+    }
+
+    @Test
+    public void testequantidadeGameMicrosoft(){
+        List<Game> microsoftGames = ServiceGame.getByPuBlisher(gameList, Publisher.MicrosoftGameStudios);
+
+        Assert.assertEquals(6,microsoftGames.size());
+    }
+
+
+}
